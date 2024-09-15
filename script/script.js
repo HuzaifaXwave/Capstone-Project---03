e6a59a249fb4fd348934d07dc7f828904b842ab5
HuzaifaXwave <mhuzaifa.xwave@gmail.com>
2024-09-15 19:53:48 +0500

    uploading files

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// add to cart functionality //
+
+let productCartBtn = document.getElementById("product-cart-button")
+let firstProductItem = document.getElementById("first-product-cart")
+let orderList = document.getElementById(("order-item-list"))
+
+productCartBtn.addEventListener('click' , function(){
+console.log('orderList.firstProductItem.value');
+
+let listItem = document.createElement("li")
+listItem.textContent = text
+
+})
